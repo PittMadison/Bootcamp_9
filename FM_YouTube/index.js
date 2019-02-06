db9f149703ce0d11a4402908471e53636e083894
Pitt <karimovpg@gmail.com>
2019-02-06 15:58:47 +0200

    added FM YouTube

---
+++ FM_YouTube/index.js
@@ -0,0 +1,52 @@
+'use strict'
+
+let body = document.querySelector('body');
+let gallery = document.createElement('ol');
+gallery.classList.add('gallery')
+body.prepend(gallery);
+let iframeDiv = document.createElement('div');
+iframeDiv.classList.add('iframeDiv');
+body.prepend(iframeDiv);
+gallery.addEventListener('click', getTube)
+
+function getFm (){
+  let URL = 'http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=412e51e107155c7ffabd155a02371cbd&format=json'
+  fetch(URL)
+  .then(res=>res.json())
+  .then(data=>{
+    console.log(data.tracks.track);
+    
+    createList(data.tracks.track)
+  })
+  .catch(err=>console.log(new Error('error in getFm function')))
+}
+getFm();
+
+function getTube (e){
+  
+if(e.target.classList.contains('push')) {
+    let query = e.target.closest('li').firstElementChild.textContent;
+    let URL = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyAGwWGzULP4Q9plH7a9ATpZW_8o2ZgJOH8&part=snippet&q=${query}&maxResults=1`;
+    fetch(URL)
+    .then(res=>res.json())
+    .then(data=>{
+      let videoID = data.items[0].id.videoId
+      iframeDiv.innerHTML =`<iframe src='https://www.youtube.com/embed/${videoID}?autoplay=1' frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>`
+      gallery.prepend(iframe)
+      })
+    .catch(err=>console.log(new Error('error in getTube function')))
+}
+}
+
+
+function createList (array){
+  gallery.innerHTML = array.reduce((acc, el) => acc + 
+  `<li class='gallery__item'>
+      <p class='gallery__itemName'>${el.artist.name} - ${el.name}</p>
+      <div class='gallery__itemContainer'>
+          <img class='gallery__itemPic' src='${el.image[3]['#text']}'>
+          <button class='push'>Youtube</button>
+      </div>
+  </li>`,'')
+  
+}
